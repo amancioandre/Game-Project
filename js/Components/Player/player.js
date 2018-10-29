c298fb1338fd3521199c5a9532103915417c1066
stormamnc <amancioandre@gmail.com>
2018-10-29 19:06:27 -0300

    try implement jump and platform collision

--- js/Components/Player/player.js
+++ js/Components/Player/player.js
@@ -1,10 +1,10 @@
 console.log('player.js loaded');
 
 class Player {
-  constructor(name, gender) {
+  constructor(name, _gender) {
     this.name = name;
     this.x = 0;
-    this.y = 0;
+    this.y = h - 100;
 
     this.spacebar = false;
     this.jumping = true;
@@ -13,16 +13,14 @@ class Player {
 
     this.speedX = 0;
     this.speedY = 0;
+    this.gravity = 4;
     this.score = 0;
 
     // this.character = new Character(gender);
   }
 
   render() {
-    // Render to the sketch
-    translate(0, h - 50);
     rect(this.x, this.y, 20, 20, 5);
-    this.move();
   }
 
   // ###################### BUILD MODULE ############################
@@ -45,7 +43,7 @@ class Player {
 
   move() {
     // FISICS
-    this.speedY += 2; // gravity
+    this.speedY += this.gravity; // gravity
 
     this.x += this.speedX;
     this.y += this.speedY;
@@ -53,27 +51,28 @@ class Player {
     this.speedY *= 0.8; // damping
     this.speedX *= 0.6; // damping
 
-
+    // DIRECTIONS
     if (this.right) {
-      this.speedX += 4;
+      this.speedX += 8;
     }
     if (this.left) {
-      this.speedX -= 4;
-    }
-
-    if (this.spacebar && this.jumping === false) {
-      this.speedY -= 50;
-      this.jumping = true;
-    } else if (this.y > 20) {
-      this.jumping = false;
-      this.y = 20;
-      this.speedY = 0;
+      this.speedX -= 8;
     }
   }
 
-  groundCollision() {
-    
-  }
-
+  jump(arr) {
+    arr.forEach((e) => {
+      if ((this.x > e.x && this.x < e.x + 20) && (this.y < -e.y + 20)) {
+        console.log(`xo: ${this.x}, xb: ${e.x}, yo: ${this.y}, yb: ${e.y}`);
+        if (this.spacebar && this.jumping === false) {
+          this.speedY -= 50;
+          this.jumping = true;
+        } else if (this.y > 0) {
+          this.jumping = false;
+          this.y = -e.y;
+          this.speedY = 0;
+        }
+      }
+    });
   }
 }
--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-window.addEventListener("keydown", function(e) {
+window.addEventListener('keydown', (e) => {
   // space and arrow keys
   if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
       e.preventDefault();
@@ -7,21 +7,74 @@ window.addEventListener("keydown", function(e) {
 
 let w;
 let h;
+let song;
 let player;
-let gravity = 5;
+let amp;
 
+let y;
+let frames = 0;
+
+let vol = [
+  {x: 60, y: 20},
+  {x: 80, y: 20},
+  {x: 100, y: 20},
+  {x: 120, y: 20},
+  {x: 140, y: 20},
+  {x: 160, y: 20},
+  {x: 180, y: 20},
+  {x: 200, y: 20},
+  {x: 220, y: 20},
+  // SECOND ROW
+  {x: 240, y: 40},
+  {x: 260, y: 40},
+  {x: 280, y: 60},
+  {x: 300, y: 60},
+  {x: 320, y: 40},
+  {x: 340, y: 40},
+  {x: 360, y: 60},
+  {x: 380, y: 40},
+  {x: 400, y: 40},
+];
 
 w = $('#game').width();
 h = $('#game').height();
+
+// function preload() {
+//   song = loadSound('../Media/music/truth inside.mp3');
+// }
 
 function setup() {
   const screen = createCanvas(w, h);
   screen.parent('game');
   player = new Player();
+  // song.play();
+  // amp = new p5.Amplitude();
+  frameRate(40);
 }
 
 function draw() {
+  translate(0, h - 50);
   background(0);
+
+  // if(amp.getLevel() !== 0){
+  //   vol.push({
+  //     y: map(amp.getLevel(), 0, 1, 0, h),
+  //     x: (20*frameCount)});
+  // }
+
+  push();
+
+  stroke(0)
   fill(255);
-  player.render(gravity);
+  vol.forEach(v => {;
+    rect(v.x, 20, 20, -v.y);
+  });
+
+  pop();
+  push();
+  fill(255, 0, 0);
+  player.render();
+  pop();
+  player.move();
+  player.jump(vol);
 }
