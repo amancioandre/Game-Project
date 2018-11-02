382b554c63e9f1acc4c5bfbc72104aed48cf5e96
stormamnc <amancioandre@gmail.com>
2018-11-02 09:10:54 -0300

    debug github pages

--- js/Setup/setup.js
+++ js/Setup/setup.js
@@ -14,32 +14,3 @@ window.addEventListener('keydown', (e) => {
     e.preventDefault();
   }
 }, false);
-
-// GAME FUNCTIONS
-function checkPlatform(bars, player) {
-  let i = parseInt((player.x) / 20 - 1);
-  // console.log(i)
-
-  if (bars[i]) {
-    if (bars[i - 1]) {
-      if (20 < bars[i - 1].y + player.y && player.x >= bars[i - 1].x + 20) {
-        player.x = bars[i].x + 20;
-        player.climb = true;
-      }
-    }
-    if (player.x >= bars[i].x - 20 && player.x <= bars[i].x + 20) {
-      if (player.y > -bars[i].y) {
-        player.jumping = false;
-        player.climb = false;
-        player.y = -bars[i].y;
-        player.speedY = -player.gravity;
-      }
-    }
-    if (bars[i + 1]) {
-      if (20 < bars[i + 1].y + player.y && player.x > bars[i + 1].x - 20) {
-        player.x = bars[i].x;
-        player.climb = true;
-      }
-    }
-  }
-}
